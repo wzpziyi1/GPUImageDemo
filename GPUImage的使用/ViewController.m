4f6b1bd7f9cb87d5e0dad405a95d8eaa53cda08c
wzpziyi1 <1204473090@qq.com>
2017-06-17 17:58:39 +0800

    美颜相机

--- GPUImage的使用/ViewController.m
+++ GPUImage的使用/ViewController.m
@@ -12,6 +12,11 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+
+@property (nonatomic, strong) GPUImageStillCamera *camera;
+
+//美白滤镜
+@property (nonatomic, strong) GPUImageBrightnessFilter *filter;
 @end
 
 @implementation ViewController
@@ -30,12 +35,60 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    [self dealupImageOne];
+    [self beautyCamera];
 }
 
 #pragma mark -- GPUImage操作
 
 
+- (IBAction)takePhoto:(id)sender
+{
+    [self.camera capturePhotoAsImageProcessedUpToFilter:self.filter withCompletionHandler:^(UIImage *processedImage, NSError *error) {
+        UIImageWriteToSavedPhotosAlbum(processedImage, nil, nil, nil);
+        self.imageView.image = processedImage;
+        [self.camera stopCameraCapture];
+    }];
+}
+
+/**
+ 美颜相机效果
+ */
+- (void)beautyCamera
+{
+    self.camera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    
+    [self.camera addTarget:self.filter];
+    
+    // 创建GPUImageView,用于显示实时画面
+    GPUImageView *showView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+    [self.view insertSubview:showView atIndex:0];
+    [self.filter addTarget:showView];
+    
+    //开始补抓画面
+    [self.camera startCameraCapture];
+}
+
+- (GPUImageStillCamera *)camera
+{
+    if (!_camera)
+    {
+        _camera = [[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPresetHigh cameraPosition:AVCaptureDevicePositionFront];
+    }
+    return _camera;
+}
+
+- (GPUImageBrightnessFilter *)filter
+{
+    if (!_filter)
+    {
+        _filter = [[GPUImageBrightnessFilter alloc] init];
+        //美白、曝光度
+        _filter.brightness = 0.7;
+    }
+    return _filter;
+}
+
+//-------分隔线-------
 /**
  高斯模糊
  */
@@ -48,6 +101,13 @@
     GPUImagePicture *picProcess = [[GPUImagePicture alloc] initWithImage:image];
     
     //添加需要处理的滤镜
+    
+    /*其他滤镜效果
+     GPUImageSepiaFilter
+     GPUImageToonFilter
+     GPUImageSketchFilter
+     GPUImageEmbossFilter
+     */
     GPUImageGaussianBlurFilter *blurFilter = [[GPUImageGaussianBlurFilter alloc] init];
     // 纹理
     blurFilter.texelSpacingMultiplier = 5;
